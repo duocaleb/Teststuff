104594c394f28a6a857780cd105a8a68abf9eb55
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2022-03-21 14:34:13 +0000

    start outlines

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(1200, 600);
   }
 
   /** 
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(45, 150, 207);
   }
 
   /**
@@ -25,11 +25,40 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
+    /*
     stroke(128);
     line(150, 25, 270, 350);  
 
     stroke(255);
     line(50, 125, 70, 50);  
+*/
+    draw_section_outlines();
+
+    
+  }
+
+  /*
+def draw_section_outlines():
+    # Draw squares on bottom
+    arcade.draw_rectangle_outline(150, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(450, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(750, 150, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(1050, 150, 300, 300, arcade.color.BLACK)
+
+    # Draw squares on top
+    arcade.draw_rectangle_outline(150, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(450, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(750, 450, 300, 300, arcade.color.BLACK)
+    arcade.draw_rectangle_outline(1050, 450, 300, 300, arcade.color.BLACK)
+  */
+
+  public void draw_section_outlines(){
+    stroke(0);
+    rect(150, 150, 300, 300);
+    rect(450, 150, 300, 300);
+    rect(750, 150, 300, 300);
+    rect(1050, 150, 300, 300);
+    
   }
   
   // define other methods down here.
