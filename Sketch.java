64b1cc82c5fec143724c97d5c275057069c781cc
caleb <duocaleb3@gmail.com>
2024-03-22 17:29:03 -0400

    Section 1

--- Sketch.java
+++ Sketch.java
@@ -76,8 +76,8 @@ public class Sketch extends PApplet {
 
     for(int intRow = 0; intRow < 30; intRow++){
       for(int intColumn = 0; intColumn < 30; intColumn++){
-        intX = 3 + 0;  //Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; //Instead of zero, calculate the proper intY location using 'intColumn'
+        intX = 3 + intColumn*10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 300 + 3 + intRow*10; //Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
