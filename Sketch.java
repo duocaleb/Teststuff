73a10facd5c60165eb2549e5beddd4b4f2727cc1
caleb <duocaleb3@gmail.com>
2024-03-22 20:02:13 -0400

    Section 5

--- Sketch.java
+++ Sketch.java
@@ -114,7 +114,21 @@ public class Sketch extends PApplet {
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 3 + intColumn*10;  //Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 3 + intRow*10; //Instead of zero, calculate the proper intY location using 'intColumn'
+
+        fill(255);
+        noStroke();
+        if (intColumn >= 30-intRow){
+          rect(intX, intY, 5, 5);
+        }
+      }
+    }
   }
 
   public void draw_section6(){
