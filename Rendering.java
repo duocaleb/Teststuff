8a95e95340f2c7583ee5d683edf45a76e879091c
caleb <duocaleb3@gmail.com>
2024-05-16 14:24:54 -0400

    my brain hurts

--- Rendering.java
+++ Rendering.java
@@ -1,6 +1,8 @@
 import processing.core.PApplet;
-import java.awt.AWTException;   
-import java.awt.Robot;   
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.ArrayList;
+import java.util.List;   
 
 
 /* 
@@ -10,7 +12,7 @@ import java.awt.Robot;
  * 
  * Author: @duocaleb
 */
-public class Rendering extends PApplet {  
+public class Rendering extends PApplet {
   //initalizing the variables and the arrays
   double dblFocalLength = 500;
   int intScreenSize = 800;
@@ -25,52 +27,56 @@ public class Rendering extends PApplet {
   // The amount of degrees it turns per half screen of distance the mouse is moved.
   double mouseSensitivity = 90;
   boolean[] isKeyPressed = new boolean[255];
+  List<Triangle3D> TriangleList3D = new ArrayList<>();
+  List<Triangle2D> TriangleList2D = new ArrayList<>();
+  float[] zBuffer = new float[intScreenSize*intScreenSize];
   Robot mouseMover;
 
-  /* 
-   * For each 3d array:
-    * Index #: function
-    * 0: List of points(3d co-ordinets)
-    * 1: Rotated pointslist
-    * 2: Edge List, matches points to points.
-    * 3: Face List:
-      *   Stores {Face1, Face2, Face3, ...}
-      *   Each face stores: {Point1, Point2, Point3, MaxZ, ColourR, ColourG, ColourB}
-    * 4: Projected Points, which are the co-ordinates of the points on the actual display
-  */
-    //cube
-    double[][][] Cube = { 
-      { {-50,-50,-50 }, {50,-50,-50 }, {50,50,-50 }, {-50,50,-50 },
-        {-50,-50,50 }, {50,-50,50 }, {50,50,50 }, {-50,50,50 } },   
-        
-        { {-50,-50,-50 }, {50,-50,-50 }, {50,50,-50 }, {-50,50,-50 },
-        {-50,-50,50 }, {50,-50,50 }, {50,50,50 }, {-50,50,50 } },
-        
-        { { 0, 1 }, { 1, 3 }, { 3, 2 }, { 2, 0 }, { 4, 5 }, { 5, 7 }, { 7, 6 }, { 6, 4 }, { 0, 4 },
-        { 1, 5 }, { 2, 6 }, { 3, 7 } },
-        
-        { {0,1,2, 0}, {1,2,3, 0}, {2,3,7, 0}, {2,6,7, 0}, {1,3,7, 0}, {1,5,7, 0}, {4,5,7, 0}, {4,6,7, 0}, {0,4,6, 0}, {0,2,6, 0}, {0,1,5, 0}, {0,4,5, 0}},
-        
-        { { 100, 100 }, { 200, 100 }, { 100, 100 }, { 200, 200 }, { 100, 100 }, { 200, 100 },
-        { 100, 100 }, { 200, 200 } },
+  public class Point3D {
+    double x;
+    double y;
+    double z;
+    Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
 
-        { {-50,-50,-50 }, {50,-50,-50 }, {50,50,-50 }, {-50,50,-50 },
-        {-50,-50,50 }, {50,-50,50 }, {50,50,50 }, {-50,50,50 } },
-      };
-    //Triangles
-    double[][][] Pyramid = { 
-        
-        {{35,-35,0}, {-35,-35,0}, {0,-35,35}, {0,-35,-35}, {0,35,0}},   
-        
-        { {35, -35,0}, {-35,-35,0}, {0,-35,35}, {0,-35,-35}, {0,35,0} },
-          
-        { { 0, 2 }, { 1, 3 }, { 3, 0 }, { 2, 1 }, {0 , 1},  {0 , 4}, {1,4}, {2,4}, {3,4}, {4,4} },
-          
-        { {0,2,1, 90,70,173}, {0,1,2, 90,70,173}, {0,2,4, 30,  20, 200}, {1,3, 4,  20,138,0}, {2,1, 4, 38,0,90}, {3,0,4, 100,100, 100} },
-          
-        { {50,-35,0}, {-50,-35,0}, {0,-35,50}, {0,-35,-50}, {0,35,0}}
-      };
+public class Point2D {
+  double x;
+  double y;
+  Point2D(double x, double y) {
+      this.x = x;
+      this.y = y;
+  }
+}
 
+  public class Triangle3D {
+    Point3D p1;
+    Point3D p2;
+    Point3D p3;
+    int c;
+    Triangle3D(Point3D point1, Point3D point2, Point3D point3, int colour){
+      this.p1 = point1;
+      this.p2 = point2;
+      this.p3 = point3;
+      this.c = colour;
+    }
+  }
+
+  public class Triangle2D{
+    Point2D p1;
+    Point2D p2;
+    Point2D p3;
+    int c;
+    Triangle2D(Point2D point1, Point2D point2, Point2D point3, int colour){
+      this.p1 = point1;
+      this.p2 = point2;
+      this.p3 = point3;
+      this.c = colour;
+    }
+  }
 
   public void settings() {
     size(intScreenSize, intScreenSize);
@@ -89,13 +95,81 @@ public class Rendering extends PApplet {
   }
   int rotate = 0;
   public void draw() {
+    TriangleList2D.clear();
+    TriangleList3D.clear();
+    for(int x = 0; x < zBuffer.length; x++){
+      zBuffer[x] =  Float.POSITIVE_INFINITY;
+    }
     clear();
     background(45, 150, 207);
-    moveChar();
-    drawCube(-50, -50, -50, 50, 0, 50, 0, 0, 0);
-    cursorMovement();
+    addCube(new Point3D(-50, -50, -50), new Point3D(50, 50, 50), 
+            color(0,0,255), 
+            color(255,255,255), 
+            color(255,255,0), 
+            color(0,128,0),
+            color(255,165,0),
+            color(255,0,0),
+            new Point3D(rotate, rotate, rotate));
+    drawFaces();
+    //drawLines(); // For testing purposes only
+    rotate++;
   }
 
+
+
+
+  
+  public void drawLines(){
+    float a = intScreenSize/2;
+    for(int x = 0; x < TriangleList2D.size(); x++){
+      float x1 = a+(float)TriangleList2D.get(x).p1.x;
+      float x2 = a+(float)TriangleList2D.get(x).p2.x;
+      float x3 = a+(float)TriangleList2D.get(x).p3.x;
+      float y1 = a-(float)TriangleList2D.get(x).p1.y;
+      float y2 = a-(float)TriangleList2D.get(x).p2.y;
+      float y3 = a-(float)TriangleList2D.get(x).p3.y;
+      line(x1,y1,x2,y2);
+      line(x2,y2,x3,y3);
+      line(x3,y3,x1,y1);
+    }
+  }
+
+  public void addCube(Point3D minPoint, Point3D maxPoint, int colour1, int colour2, int colour3, int colour4, int colour5, int colour6, Point3D Rotation){
+    Point3D[] PointList = new Point3D[8];
+    int[] colourList = new int[6];
+    for(int x = 0; x < 8; x++){
+      // Defining a new point cause it doesnt work otherwise
+      PointList[x] = new Point3D(0,0,0);
+    }
+    int[][] Connections = {{0,1,2}, {1,2,3}, {2,3,7}, {2,6,7}, {1,3,7}, {1,5,7}, {4,5,7}, {4,6,7}, {0,4,6}, {0,2,6}, {0,1,5}, {0,4,5}};
+    colourList[0] = colour1;
+    colourList[1] = colour2;
+    colourList[2] = colour3;
+    colourList[3] = colour4;
+    colourList[4] = colour5;
+    colourList[5] = colour6;
+    double minX = minPoint.x;
+    double minY = minPoint.y;
+    double minZ = minPoint.z;
+    double maxX = maxPoint.x;
+    double maxY = maxPoint.y;
+    double maxZ = maxPoint.z;
+
+    for(int x = 0; x < 4; x += 1){
+      PointList[x*2].x = minX;
+      PointList[x*2+1].x = maxX;
+      PointList[(int)Math.floor(x/2)*2+x].y = minY;
+      PointList[(int)Math.floor(x/2)*2+x+2].y = maxY;
+      PointList[x].z = minZ;
+      PointList[x+4].z = maxZ;
+    }
+    for(int x = 0; x < 8; x++){
+      rotatePoint(PointList[x], Rotation.x, Rotation.y, Rotation.z);
+    }
+    for(int a = 0; a < 12; a++){
+      TriangleList3D.add(new Triangle3D(PointList[Connections[a][0]], PointList[Connections[a][1]], PointList[Connections[a][2]],colourList[(int)a/2]));
+    }
+  }
 
   public void cursorMovement(){
     if(mouseCenteredX == 0 && mouseCenteredY == 0){
@@ -120,11 +194,15 @@ public class Rendering extends PApplet {
   }
 
   public void keyPressed() {
-    isKeyPressed[(int)key] = true;
+    if(key != 65535){
+      isKeyPressed[(int)key] = true;
+    }
   }
   
   public void keyReleased() {
-    isKeyPressed[(int)key] = false;
+    if(key != 65535){
+      isKeyPressed[(int)key] = false;
+    }
   }
 
   public void moveChar() {
@@ -144,33 +222,6 @@ public class Rendering extends PApplet {
     }
   }
 
-  public void drawCube(double xStart, double yStart, double zStart, double xEnd, double yEnd, double zEnd, double rotationXZ, double rotationXY, double rotationYZ){
-    double xPoint = (xEnd-xStart)/2;
-    double yPoint = (yEnd-yStart)/2;
-    double zPoint = (zEnd-zStart)/2;
-    for(int x = 0; x < 4; x += 1){
-      Cube[0][x*2][0] = xStart;
-      Cube[0][x*2+1][0] = xEnd;
-      Cube[0][(int)Math.floor(x/2)*2+x][1] = yStart;
-      Cube[0][(int)Math.floor(x/2)*2+x+2][1] = yEnd;
-      Cube[0][x][2] = zStart;
-      Cube[0][x+4][2] = zEnd;
-    }
-    for(int x = 0; x < 4; x += 1){
-      Cube[1][x*2][0] = -xPoint;
-      Cube[1][x*2+1][0] = xPoint;
-      Cube[1][(int)Math.floor(x/2)*2+x][1] = -yPoint;
-      Cube[1][(int)Math.floor(x/2)*2+x+2][1] = yPoint;
-      Cube[1][x][2] = -zPoint;
-      Cube[1][x+4][2] = zPoint;
-    }
-    setRotation(Cube, rotationXZ,rotationXY,rotationYZ);
-    rotateAroundCamera(Cube, mouseRotationX, mouseRotationY);
-    projectPoints(Cube);
-    drawFaces(Cube);
-    drawLines(Cube);
-  }
-
   public void rotateAroundCamera(double[][][] array, double angleToTurnX, double angleToTurnY){
     
   }
@@ -178,83 +229,113 @@ public class Rendering extends PApplet {
   /*
    * Project the points onto the screen.
    */
-  public void projectPoints(double[][][] array) {
-    for (int x = 0; x < array[0].length; x++) {
-      double z = array[0][x][2];
-      if (dblFocalLength - array[0][x][2] <= 0){
+  public void projectPoints() {
+    for (int a = 0; a < TriangleList3D.size(); a++) {
+      double z1 = TriangleList3D.get(a).p1.z;
+      double z2 = TriangleList3D.get(a).p2.z;
+      double z3 = TriangleList3D.get(a).p3.z;
+      if (dblFocalLength - z1 <= 0){
         // Prevents an asymptote from ruining everything
-        z = dblFocalLength - 1;
+        z1 = dblFocalLength - 1;
       }
-      array[4][x][0] = (array[0][x][0] * dblFocalLength) / (dblFocalLength - z);
-      array[4][x][1] = (array[0][x][1] * dblFocalLength) / (dblFocalLength - z);
-      
+      if (dblFocalLength - z2 <= 0){
+        z2 = dblFocalLength - 1;
+      }
+      if (dblFocalLength - z3 <= 0){
+        z3 = dblFocalLength - 1;
+      }
+      double x1 = (TriangleList3D.get(a).p1.x * dblFocalLength) / (dblFocalLength - z1);
+      double x2 = (TriangleList3D.get(a).p2.x * dblFocalLength) / (dblFocalLength - z2);
+      double x3 = (TriangleList3D.get(a).p3.x * dblFocalLength) / (dblFocalLength - z3);
+      double y1 = (TriangleList3D.get(a).p1.y * dblFocalLength) / (dblFocalLength - z1);
+      double y2 = (TriangleList3D.get(a).p2.y * dblFocalLength) / (dblFocalLength - z2);
+      double y3 = (TriangleList3D.get(a).p3.y * dblFocalLength) / (dblFocalLength - z3);
+
+      TriangleList2D.add(new Triangle2D(new Point2D(x1, y1), new Point2D(x2,y2), new Point2D(x3,y3), TriangleList3D.get(a).c));
     }
-    
   }
 
-  public void drawLines(double[][][] array) {
-    stroke(0,0,0);
-    for (int x = 0; x < (int)array[2].length; x++) {
-      line(intScreenSize / 2 + (int) array[4][(int)array[2][x][0]][0], intScreenSize / 2 - (int) array[4][(int)array[2][x][0]][1],
-           intScreenSize / 2 + (int) array[4][(int)array[2][x][1]][0], intScreenSize / 2 - (int) array[4][(int)array[2][x][1]][1]);
-    }
+  public void rotateTriangle(Triangle3D triangle, double angleXZ, double angleXY, double angleYZ){
+    rotatePoint(triangle.p1, angleXZ, angleXY, angleYZ);
+    rotatePoint(triangle.p2, angleXZ, angleXY, angleYZ);
+    rotatePoint(triangle.p2, angleXZ, angleXY, angleYZ);
   }
 
-  public void setRotation(double[][][] array, double angleXZ, double angleXY, double angleYZ) {
-    // Technically, this is add rotation, not set. However, since the shapes are being reset each frame, they are the same thing anyways.
-
+  public void rotatePoint(Point3D P, double angleXZ, double angleXY, double angleYZ) {
     angleXZ = Math.toRadians(angleXZ);
     angleXY = Math.toRadians(angleXY);
     angleYZ = Math.toRadians(angleYZ);
-
-    for (int count = 0; count < array[0].length; count++) {
-      double x = array[1][count][0];
-      double z = array[1][count][2];
-      double xInit = array[1][count][0];
-      double zInit = array[1][count][2];
-      array[1][count][0] = x * Math.cos(angleXZ) - z * Math.sin(angleXZ);
-      array[1][count][2] = x * Math.sin(angleXZ) + z * Math.cos(angleXZ);
-
-      array[0][count][0] += array[1][count][0] - xInit;
-      array[0][count][2] += array[1][count][2] - zInit;
-    }
-
-    for (int count = 0; count < array[0].length; count++) {
-      double x = array[1][count][0];
-      double y = array[1][count][1];
-
-      double xInit = array[1][count][0];
-      double yInit = array[1][count][1];
-      array[1][count][0] = x * Math.cos(angleXY) - y * Math.sin(angleXY);
-      array[1][count][1] = x * Math.sin(angleXY) + y * Math.cos(angleXY);
-
-      array[0][count][0] += array[1][count][0] - xInit;
-      array[0][count][1] += array[1][count][1] - yInit;
-    }
-
-    for (int count = 0; count < array[0].length; count++) {
-      double z = array[1][count][2];
-      double y = array[1][count][1];
-
-      double zInit = array[1][count][2];
-      double yInit = array[1][count][1];
-
-      array[1][count][2] = z * Math.cos(angleYZ) - y * Math.sin(angleYZ);
-      array[1][count][1] = z * Math.sin(angleYZ) + y * Math.cos(angleYZ);
-
-      array[0][count][2] += array[1][count][2] - zInit;
-      array[0][count][1] += array[1][count][1] - yInit;
-    }
+    double x;
+    double z;
+    double y;
+    x = P.x;
+    z = P.z;
+    P.x = x * Math.cos(angleXZ) - z * Math.sin(angleXZ);
+    P.z = x * Math.sin(angleXZ) + z * Math.cos(angleXZ);
+    x = P.x;
+    y = P.y;
+    P.x = x * Math.cos(angleXY) - y * Math.sin(angleXY);
+    P.y = x * Math.sin(angleXY) + y * Math.cos(angleXY);
+    z = P.z;
+    y = P.y;
+    P.z = z * Math.cos(angleYZ) - y * Math.sin(angleYZ);
+    P.y = z * Math.sin(angleYZ) + y * Math.cos(angleYZ);
   }
 
-  public void drawFaces(double[][][] array){
-    noStroke();
-    for (int x = array[3].length - 1; x > -1; x--) {
-      beginShape();
-      for (int y = 0; y < 3; y++) { 
-        vertex(intScreenSize/2 + (float)array[4][(int)array[3][x][y]][0], intScreenSize/2 - (float)array[4][(int)array[3][x][y]][1]);
+  public double area(int x1, int y1, int x2, int y2, int x3, int y3){
+    return Math.abs((x1*(y2-y3) + x2*(y3-y1)+x3*(y1-y2))/2.0);
+  }
+
+  public double[] calculateBarycentricCoordinates(Point2D point, Point2D[] vertices) {
+    double[] baryCoords = new double[3];
+    double totalArea = area((int)vertices[0].x, (int)vertices[0].y, (int)vertices[1].x, (int)vertices[1].y, (int)vertices[2].x, (int)vertices[2].y);
+    baryCoords[0] = area((int)point.x, (int)point.y, (int)vertices[1].x, (int)vertices[1].y, (int)vertices[2].x, (int)vertices[2].y) / totalArea;
+    baryCoords[1] = area((int)vertices[0].x, (int)vertices[0].y, (int)point.x, (int)point.y, (int)vertices[2].x, (int)vertices[2].y) / totalArea;
+    baryCoords[2] = area((int)vertices[0].x, (int)vertices[0].y, (int)vertices[1].x, (int)vertices[1].y, (int)point.x, (int)point.y) / totalArea;
+
+    return baryCoords;
+  }
+
+public double interpolateDepth(Point2D point, Point2D[] vertices, double[] depths) {
+  double[] baryCoords = calculateBarycentricCoordinates(point, vertices);
+  double interpolatedDepth = baryCoords[0] * depths[0] + 
+                             baryCoords[1] * depths[1] +
+                             baryCoords[2] * depths[2];
+
+  return interpolatedDepth;
+}
+
+  public void drawFaces(){
+    // This took me over a month + reading more than 20 articles on 3d rendering, asking reddit, looking through wikipedia and looking thorugh blogs just to understand how this works and how to implement it.
+    projectPoints();
+    for(int r = 0; r < TriangleList2D.size(); r++){
+      Point2D p1 = TriangleList2D.get(r).p1;
+      Point2D p2 = TriangleList2D.get(r).p2;
+      Point2D p3 = TriangleList2D.get(r).p3;
+      int x1 = (int)p1.x;
+      int x2 = (int)p2.x;
+      int x3 = (int)p3.x;
+      int y1 = (int)p1.y;
+      int y2 = (int)p2.y;
+      int y3 = (int)p3.y;
+      double A = area (x1, y1, x2, y2, x3, y3);
+      Point2D[] pointsList = {p1,p2,p3};
+      double[] depths = {TriangleList3D.get(r).p1.z,TriangleList3D.get(r).p2.z,TriangleList3D.get(r).p3.z};
+      // proud to say i came up with the bounding box without looking it up(it's pretty much the only concept in this method i didnt look up)
+      for(int x = (int)Math.min(Math.min(p1.x, p2.x),p3.x); x < Math.max(Math.max(p1.x, p2.x), p3.x); x++){
+        for(int y = (int)Math.min(Math.min(p1.y,p2.y),p3.y); y < Math.max(Math.max(p1.y, p2.y), p3.y); y++){
+        double A1 = area (x, y, x2, y2, x3, y3);
+        double A2 = area (x1, y1, x, y, x3, y3);
+        double A3 = area (x1, y1, x2, y2, x, y);
+          if(A == A1 + A2 + A3){
+            double z = -interpolateDepth(new Point2D(x, y), pointsList, depths);
+            if(z < zBuffer[((intScreenSize/2-y)-1)*intScreenSize+(intScreenSize/2+x)]){
+              zBuffer[((intScreenSize/2-y)-1)*intScreenSize+(intScreenSize/2+x)] = (float)z;
+              set(intScreenSize/2 + x, intScreenSize/2 - y, TriangleList2D.get(r).c);
+            }
+          }
+        }
       }
-      endShape(CLOSE);
     }
   }
 }
--- tester.java
+++ tester.java
@@ -10,94 +10,9 @@ public class tester extends PApplet {
     size(800, 600);
   }
   public void setup() {
-    
-    
-    // Define the vertices of the cube
-    vertices = new float[][] {
-      {-50, -50, -50},
-      {50, -50, -50},
-      {50, 50, -50},
-      {-50, 50, -50},
-      {-50, -50, 50},
-      {50, -50, 50},
-      {50, 50, 50},
-      {-50, 50, 50}
-    };
-    
-    // Define the edges of the cube
-    edges = new int[][] {
-      {0, 1}, {1, 2}, {2, 3}, {3, 0},
-      {4, 5}, {5, 6}, {6, 7}, {7, 4},
-      {0, 4}, {1, 5}, {2, 6}, {3, 7}
-    };
+
   }
-  
   public void draw() {
-    background(255);
-    translate(width/2, height/2);
-    
-    // Rotate the cube around the camera
-    rotateCube(angleX, angleY, angleZ);
-    
-    // Draw the edges of the cube
-    for (int[] edge : edges) {
-      int v1 = edge[0];
-      int v2 = edge[1];
-      float[] start = project(vertices[v1]);
-      float[] end = project(vertices[v2]);
-      line(start[0], start[1], end[0], end[1]);
-    }
-    
-    // Update rotation angles for animation
-    angleX = (float)0.01;
-    angleY = (float)0.01;
-  }
-  
-  // Rotate the cube around the camera
-  void rotateCube(float x, float y, float z) {
-    float cameraX = 1000;
-    float cameraY = 0;
-    float cameraZ = -1000; // Adjust this value to change the camera distance from the cube
-    
-    for (int i = 0; i < vertices.length; i++) {
-      float[] vertex = vertices[i];
-      float x0 = vertex[0] - cameraX;
-      float y0 = vertex[1] - cameraY;
-      float z0 = vertex[2] - cameraZ;
-      
-      // Rotate around X axis
-      float y1 = y0 * cos(x) - z0 * sin(x);
-      float z1 = y0 * sin(x) + z0 * cos(x);
-      y0 = y1;
-      z0 = z1;
-      
-      // Rotate around Y axis
-      float x1 = x0 * cos(y) + z0 * sin(y);
-      z1 = -x0 * sin(y) + z0 * cos(y);
-      x0 = x1;
-      z0 = z1;
-      
-      // Rotate around Z axis
-      x1 = x0 * cos(z) - y0 * sin(z);
-      y1 = x0 * sin(z) + y0 * cos(z);
-      x0 = x1;
-      y0 = y1;
-      
-      vertices[i][0] = x0 + cameraX;
-      vertices[i][1] = y0 + cameraY;
-      vertices[i][2] = z0 + cameraZ;
-    }
-  }
-  
-  // Project a 3D point onto a 2D canvas
-  float[] project(float[] vertex) {
-    float fov = PI/3;
-    float cameraZ = (float)(height / Math.tan(fov / 2.0));
-    float aspect = (float)(width) / (float)(height);
-    float[] projected = new float[2];
-    projected[0] = vertex[0] * cameraZ / (vertex[2] + cameraZ)* aspect;
-    projected[1] = vertex[1] * cameraZ / (vertex[2] + cameraZ);
-    return projected;
-  }
+   }
   
 }
